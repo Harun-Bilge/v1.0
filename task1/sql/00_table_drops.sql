37e5d9b02758705822dc5540fff9e9cea7e3c009
Harun Bilge <harunbilge@icloud.com>
2025-11-17 08:08:45 +0300

    Delete task1/sql directory

--- task1/sql/00_table_drops.sql
+++
@@ -1,8 +0,0 @@
--- Drop tables if needed
-IF OBJECT_ID('dbo.days') IS NOT NULL DROP TABLE dbo.days;
-IF OBJECT_ID('dbo.parameters') IS NOT NULL DROP TABLE dbo.parameters;
-IF OBJECT_ID('dbo.installs') IS NOT NULL DROP TABLE dbo.installs;
-IF OBJECT_ID('dbo.retention_model') IS NOT NULL DROP TABLE dbo.retention_model;
-IF OBJECT_ID('dbo.dau') IS NOT NULL DROP TABLE dbo.dau;
-IF OBJECT_ID('dbo.revenue_prep') IS NOT NULL DROP TABLE dbo.revenue_prep;
-IF OBJECT_ID('dbo.revenue') IS NOT NULL DROP TABLE dbo.revenue;
